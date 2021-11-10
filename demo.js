9c4197a1024f02cd60ec0930b7ef043f499b21b1
sivakoushik <sivakoushik.d@autorabit.com>
2021-11-10 12:20:58 +0530

    adding file

---
+++ demo.js
@@ -0,0 +1,20 @@
+function ValidateIPaddress(ip)
+{
+ if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip))
+  {
+   console.log('IP Address verified');
+  }
+  else
+  {
+   alert("You have entered an invalid IP address!")
+  }
+  if (true) {
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+    console.log('sdggg');
+  }
+  if(true){
+    console.log("sdggg");
+  }
+}
